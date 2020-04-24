faab5b24e02e3d84802129ebe51975e97959e3ae
Alexey Sobolev <sobolev@mail.de>
2020-04-24 21:54:33 +0200

    Initialize asset config remotely

--- asset/src/routes/init.ts
+++ asset/src/routes/init.ts
@@ -1,23 +1,30 @@
-import { IConfiguration } from "../models/configuration/IConfiguration";
+// import path from 'path';
+import assetConfig from '../asset.config.json';
+import { readAllData, writeData } from '../utils/databaseHelper';
 
 /**
  * Initialise the database.
  * @param config The configuration.
  * @returns The response.
  */
-export async function init(config: IConfiguration): Promise<string[]> {
-    let log = "Initializing\n";
+export async function init(): Promise<string[]> {
+    let log = 'Initializing\n';
 
     try {
+        await readAllData('asset');
+        await new Promise(resolve => setTimeout(resolve, 2000));
+        const { assetWallet, ...config } = assetConfig;
+        await writeData('wallet', assetWallet);
+        await writeData('asset', config);
     } catch (err) {
         log += `Failed\n${err.toString()}\n`;
     }
 
-    if (log.indexOf("Failed") < 0) {
-        log += "Initialization Succeeded";
+    if (log.indexOf('Failed') < 0) {
+        log += 'Initialization Succeeded';
     } else {
-        log += "Initialization Failed";
+        log += 'Initialization Failed';
     }
 
-    return log.split("\n");
+    return log.split('\n');
 }
