6d3b8fdcc9aaaf96371d8800c48e77a30198b32a
Alexey Sobolev <sobolev@mail.de>
2020-04-28 23:28:02 +0200

    updated asset initialization

--- asset/src/routes/init.ts
+++ asset/src/routes/init.ts
@@ -1,30 +1,39 @@
 // import path from 'path';
 import assetConfig from '../asset.config.json';
-import { readAllData, writeData } from '../utils/databaseHelper';
+import { readData, readAllData, writeData } from '../utils/databaseHelper';
+import { EncryptionService, IKeys } from '../utils/encryptionHelper';
+import { log } from '../utils/loggerHelper';
 
 /**
  * Initialise the database.
  * @param config The configuration.
- * @returns The response.
  */
-export async function init(): Promise<string[]> {
-    let log = 'Initializing\n';
-
+export async function init(): Promise<void> {
     try {
+        // Verify the config is received from user
+        // Use trusted DID to verify
+        
+        // Register asset with Marketplace
+        // asset ID, type, MAM channel details, public key in a local database
+        // replies with MAM root/DID, where public key is stored
+
         await readAllData('asset');
         await new Promise(resolve => setTimeout(resolve, 2000));
+        await log('Initializing...');
         const { assetWallet, ...config } = assetConfig;
         await writeData('wallet', assetWallet);
         await writeData('asset', config);
+
+        const keys: any = await readData('keys');
+        if (!keys || !keys.privateKey) {
+            const encryptionService = new EncryptionService();
+            const keyPair: IKeys = encryptionService.generateKeys();
+            await writeData('keys', keyPair);
+        }
     } catch (err) {
-        log += `Failed\n${err.toString()}\n`;
+        await log(`Initialization Failed ${err.toString()}`);
+        return;
     }
 
-    if (log.indexOf('Failed') < 0) {
-        log += 'Initialization Succeeded';
-    } else {
-        log += 'Initialization Failed';
-    }
-
-    return log.split('\n');
+    await log('Initialization Succeeded');
 }
