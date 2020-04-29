da16796365432c135b997f448cdbc7e3dde83ff6
Alexey Sobolev <sobolev@mail.de>
2020-04-29 18:11:16 +0200

    Fixed registration endpoint, added encryption

--- marketplace/src/routes/register.ts
+++ marketplace/src/routes/register.ts
@@ -1,16 +1,30 @@
 import { readData, writeData } from '../utils/databaseHelper';
 import { log } from '../utils/loggerHelper';
 import { HttpError } from '../errors/httpError';
+import { EncryptionService, IReceivedMessagePayload } from '../utils/encryptionHelper';
 
 export async function register(_: any, request: any): Promise<any> {
-    console.log('Got registration');
     try {
-        if (request) {
-            const asset: any = await readData('asset', 'assetId', request.assetId);
-            if (!asset) {
-                await writeData('asset', request);
+        if (request && request?.encrypted) {
+            const keys: any = await readData('keys');
+            if (keys && keys.privateKey) {
+                const encryptionService = new EncryptionService();
+
+                const decrypted: IReceivedMessagePayload = encryptionService.privateDecrypt(
+                    keys.privateKey, request?.encrypted
+                );
+    
+                const verificationResult: boolean = encryptionService.verifySignature(
+                    decrypted?.message?.assetPublicKey, decrypted?.message, decrypted?.signature
+                );  
+    
+                const asset: any = await readData('asset', 'assetId', decrypted?.message?.assetId);
+                if (verificationResult && !asset) {
+                    await writeData('asset', decrypted?.message);
+                }
+                return { success: true };
             }
-            return { success: true };
+            throw new HttpError('No marketplace key', 404);
         } else {
             throw new HttpError('No data found', 404);
         }
