fd49daa030183c37043a76d88d65c975906afa42
Alexey Sobolev <sobolev@mail.de>
2020-04-29 14:50:44 +0200

    added asset registration with marketplace

--- asset/src/routes/init.ts
+++ asset/src/routes/init.ts
@@ -1,8 +1,9 @@
 // import path from 'path';
 import assetConfig from '../asset.config.json';
 import { readData, readAllData, writeData } from '../utils/databaseHelper';
-import { EncryptionService, IKeys } from '../utils/encryptionHelper';
+import { EncryptionService } from '../utils/encryptionHelper';
 import { log } from '../utils/loggerHelper';
+import { sendRequest } from '../utils/communicationHelper';
 
 /**
  * Initialise the database.
@@ -13,10 +14,6 @@ export async function init(): Promise<void> {
         // Verify the config is received from user
         // Use trusted DID to verify
         
-        // Register asset with Marketplace
-        // asset ID, type, MAM channel details, public key in a local database
-        // replies with MAM root/DID, where public key is stored
-
         await readAllData('asset');
         await new Promise(resolve => setTimeout(resolve, 2000));
         await log('Initializing...');
@@ -24,11 +21,29 @@ export async function init(): Promise<void> {
         await writeData('wallet', assetWallet);
         await writeData('asset', config);
 
-        const keys: any = await readData('keys');
+        let keys: any = await readData('keys');
         if (!keys || !keys.privateKey) {
             const encryptionService = new EncryptionService();
-            const keyPair: IKeys = encryptionService.generateKeys();
-            await writeData('keys', keyPair);
+            keys = encryptionService.generateKeys();
+            await writeData('keys', keys);
+        }
+
+        // Register asset with Marketplace
+        if (config.marketplaceAPI) {
+            const assetPayload = {
+                assetId: config.assetId, 
+                assetOwner: config.assetOwner, 
+                assetName: config.assetName, 
+                assetPublicKey: keys.publicKey, 
+                deviceUUID: config.deviceUUID, 
+                location: config.location,
+                type: config.type, 
+                network: config.network
+            };
+            const response = await sendRequest('/register', assetPayload);
+            if (!response || !response.success) {
+                throw new Error('Asset registration with marketplace failed');
+            }
         }
     } catch (err) {
         await log(`Initialization Failed ${err.toString()}`);
