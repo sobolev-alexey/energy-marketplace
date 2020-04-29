504dc2b873012d97f1c3a2722e604cd2587e8cb2
Alexey Sobolev <sobolev@mail.de>
2020-04-29 19:06:44 +0200

    Added routes

--- marketplace/src/index.ts
+++ marketplace/src/index.ts
@@ -6,7 +6,9 @@ import { init } from './routes/init';
 import { AppHelper } from './utils/appHelper';
 
 const routes: IRoute[] = [
-    { path: '/register', method: 'post', func: 'register' }
+    { path: '/register', method: 'post', func: 'register' },
+    { path: '/request', method: 'post', func: 'request' },
+    { path: '/offer', method: 'post', func: 'offer' }
     // { path: '/v0/fetch', method: 'post', folder: 'v0', func: 'fetch' }
 ];
 
