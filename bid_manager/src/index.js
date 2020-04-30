39f6279d76bb624358470925abf33fd216c700a1
Alexey Sobolev <sobolev@mail.de>
2020-04-30 17:18:35 +0200

    Bid manager business logic

--- bid_manager/src/index.js
+++ bid_manager/src/index.js
@@ -1,67 +1,92 @@
 const express = require('express');
-const cors = require('cors');
 const bodyParser = require('body-parser');
-const { writeData, readData, readAllData, removeData } = require('./database');
+const { writeData, readAllData, removeData } = require('./database');
 
 const app = express();
 app.use(bodyParser.json({ limit: '100mb' }));
 app.use(bodyParser.urlencoded({ limit: '100mb', extended: true }));
 app.use(bodyParser.json());
 
+app.post('/offer', async (req, res) => {
+    try {
+        console.log('Got offer');
+        console.log(req.body);
+        const match = await findMatch('request', req.body);
+        if (match) { // if there is a match, return match
+            await removeData('request', 'transactionId', match.request.transactionId);
+            res.json({ status: 'success', match });
+        } else { // if no match, store offer
+            await writeData('offer', req.body);
+            res.json({ status: 'success' });
+        }
+    } catch (e) {
+        console.error(e);
+        res.json({ status: 'offer failure', error: JSON.stringify(e) });
+    }
+});
 
-try {
+app.post('/request', async (req, res) => {
+    try {
+        console.log('Got request');
+        console.log(req.body);
+        const match = await findMatch('offer', req.body);
+        if (match) { // if there is a match, return match
+            await removeData('offer', 'transactionId', match.offer.transactionId);
+            res.json({ status: 'success', match });
+        } else { // if no match, store request
+            await writeData('request', req.body);
+            res.json({ status: 'success' });
+        }
+    } catch (e) {
+        console.error(e);
+        res.json({ status: 'request failure', error: JSON.stringify(e) });
+    }
+});
 
-} catch (error) {
-    console.error(error);
+const findMatch = async (table, payload) => {
+    try {
+        console.log('Looking for match', table, payload);
+        const data = await readAllData(table);
+        // Rules:
+        // energy amount offered >= energy amount requested
+        // energy price offered <= energy price requested
+        // offer assetId !== request assetId
+        switch (table) {
+            case 'offer':
+                const match = data.find(offer => 
+                    offer.assetId !== payload.assetId &&
+                    offer.energyAmount >= payload.energyAmount &&
+                    offer.price <= payload.price
+                );
+
+                console.log('Found matching offer');
+                console.log(match, payload);
+
+                return {
+                    offer: match,
+                    request: payload
+                };
+            case 'request':
+                const match = data.find(request => 
+                    request.assetId !== payload.assetId &&
+                    request.energyAmount <= payload.energyAmount &&
+                    request.price >= payload.price
+                );
+
+                console.log('Found matching request');
+                console.log(match, payload);
+
+                return {
+                    offer: payload,
+                    request: match
+                };
+            default:
+                return;
+        }
+
+    } catch (error) {
+        console.error(error);
+    }
 }
-
-/*
-Get company details
-*/
-app.get('/company', cors(corsOptions), async (req, res) => {
-    try {
-        const companyNumber = req.query.company;
-        if (companyNumber) {
-            const data = await readData('company', companyNumber);
-            res.json({
-                status: 'success',
-                data
-            });
-        } else {
-            const data = await readAllData('company');
-            res.json({
-                status: 'success',
-                data
-            });
-        }
-    } catch (e) {
-        console.error(e);
-        res.json({
-            status: 'failure',
-            error: JSON.stringify(e)
-        });
-    }
-});
-
-app.post('/activate', cors(corsOptions), async (req, res) => {
-    // res.setHeader('Access-Control-Allow-Methods', 'POST,OPTIONS');
-    // res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-    try {
-        const companyNumber = req.body.company;
-        if (companyNumber) {
-            const company = await readData('company', companyNumber);
-            await createOrUpdateCompany({ ...company, CompanyStatus: 'Active' });
-            res.json({
-                status: 'success'
-            });
-        }
-    } catch (e) {
-        console.error(e);
-        res.json({
-            status: 'failure',
-            error: JSON.stringify(e)
-        });
-    }
-});
 
 module.exports = app;
