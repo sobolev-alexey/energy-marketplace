616e9a906207217644b8101e4342ab7396958923
Alexey Sobolev <sobolev@mail.de>
2020-04-30 21:34:00 +0200

    Added consumer asset

---
+++ asset_consumer/src/index.ts
@@ -0,0 +1,47 @@
+
+import { LoadBalancerSettings, RandomWalkStrategy } from '@iota/client-load-balancer';
+import { Server } from 'http';
+import SocketIO from 'socket.io';
+import { ServiceFactory } from './factories/serviceFactory';
+import { IRoute } from './models/app/IRoute';
+import { init } from './routes/init';
+import { message } from './socketSubscriptions/message';
+import { AppHelper } from './utils/appHelper';
+import { BusinessLogic } from './utils/businessLogicHelper';
+
+const routes: IRoute[] = [
+    { path: '/init', method: 'get', func: 'init' },
+    { path: '/v0/publish', method: 'post', folder: 'v0', func: 'publish' },
+    { path: '/v0/fetch', method: 'post', folder: 'v0', func: 'fetch' }
+];
+
+AppHelper.build(routes, async (app, config, websocketPort) => {
+    const mainNetLoadBalancerSettings: LoadBalancerSettings = {
+        nodeWalkStrategy: new RandomWalkStrategy(config.mainNetNodes),
+        timeoutMs: 10000
+    };
+    ServiceFactory.register('mainnet-load-balancer-settings', () => mainNetLoadBalancerSettings);
+
+    const devNetLoadBalancerSettings: LoadBalancerSettings = {
+        nodeWalkStrategy: new RandomWalkStrategy(config.devNetNodes),
+        timeoutMs: 10000
+    };
+    ServiceFactory.register('devnet-load-balancer-settings', () => devNetLoadBalancerSettings);
+
+    const server = new Server(app);
+    const socketServer = SocketIO(server);
+    server.listen(websocketPort);
+    socketServer.on('connection', socket => {
+        console.log('marketplace connected');
+        // socket.on('subscribe', data => socket.emit('subscribe', transactionsSubscribe(config, socket)));
+        socket.on('message', message);
+        socket.on('disconnect', () => console.log('marketplace disconnected'));
+    });
+
+    ServiceFactory.register('businessLogic', () => new BusinessLogic());
+
+    // tslint:disable-next-line:no-unused-expression
+    new BusinessLogic();
+});
+
+init();
---
+++ asset_consumer/src/utils/mamHelper.ts
@@ -0,0 +1,119 @@
+import { composeAPI, LoadBalancerSettings } from '@iota/client-load-balancer';
+import { 
+    asciiToTrytes,
+    trytesToAscii 
+} from '@iota/converter';
+import {
+    createChannel,
+    createMessage,
+    IMamMessage,
+    mamAttach,
+    mamFetchAll
+} from '@iota/mam.js';
+import crypto from 'crypto';
+import { chunkSize, defaultDepth, defaultMwm, defaultSecurity, mamMode, tag } from '../config.json';
+import { ServiceFactory } from '../factories/serviceFactory';
+import { readData, writeData } from './databaseHelper';
+
+// An enumerator for the different MAM Modes. Prevents typos in regards to the different modes.
+export enum MAM_MODE {
+    PRIVATE = 'private',
+    PUBLIC = 'public',
+    RESTRICTED = 'restricted'
+}
+
+export interface IMamState {
+    id?: string;
+    seed: string;
+    mode: MAM_MODE;
+    sideKey: string;
+    security: number;
+    start: number;
+    count: number;
+    nextCount: number;
+    index: number;
+    nextRoot: string;
+}
+
+// Random Key Generator
+const generateRandomKey = length => {
+    const charset = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ9';
+    let seed = '';
+    while (seed.length < length) {
+        const byte = crypto.randomBytes(1);
+        if (byte[0] < 243) {
+            seed += charset.charAt(byte[0] % 27);
+        }
+    }
+    return seed;
+};
+
+// Publish to tangle
+export const publish = async (transactionId, packet) => {
+    try {
+        let mamState;
+        let secretKey;
+
+        const config: any = await readData('asset');
+        const loadBalancerSettings = ServiceFactory.get<LoadBalancerSettings>(
+            `${config?.network}-load-balancer-settings`
+        );
+        const api = composeAPI(loadBalancerSettings);
+        const mamStateFromDB: any = await readData('mam', 'transactionId', transactionId);
+
+        if (mamStateFromDB) {
+            secretKey = mamStateFromDB?.sideKey;
+            mamState = mamStateFromDB;
+            mamState.index = mamStateFromDB?.keyIndex;
+            console.log('RETRIEVED MAM state', mamState);
+        } else {
+            // Set channel mode & update key
+            secretKey = generateRandomKey(81);
+            mamState = createChannel(generateRandomKey(81), defaultSecurity, MAM_MODE[mamMode], secretKey);
+            console.log('INITIAL MAM state', mamState);
+
+        }
+
+        // Create MAM Payload - STRING OF TRYTES
+        const trytes: string = asciiToTrytes(JSON.stringify(packet));
+        const message: IMamMessage = createMessage(mamState, trytes);
+        const bundle = await mamAttach(api, message, defaultDepth, defaultMwm, tag);
+        const root = mamStateFromDB && mamStateFromDB?.root ? mamStateFromDB.root : message.root;
+
+        // Attach the payload
+        if (bundle && bundle.length && bundle[0].hash) {
+            // Save new mamState
+            await new Promise(resolve => setTimeout(resolve, 2000));
+            await writeData('mam', { transactionId, root, ...mamState });
+            return { transactionId, root, ...mamState };
+        }
+        return null;
+    } catch (error) {
+        console.log('MAM publish', error);
+        throw new Error(error);
+    }
+};
+
+export const fetch = async (assetId, transactionId) => {
+    try {
+        const config: any = await readData('asset', 'assetId', assetId, 1);
+        const loadBalancerSettings = ServiceFactory.get<LoadBalancerSettings>(
+            `${config?.network}-load-balancer-settings`
+        );
+        const api = composeAPI(loadBalancerSettings);
+
+        const channelState: any = await readData('mam', 'transactionId', transactionId, 1);
+        const fetched = await mamFetchAll(api, channelState.root, channelState.mode, channelState.sideKey, chunkSize);
+        const result = [];
+        
+        if (fetched && fetched.length > 0) {
+            for (let i = 0; i < fetched.length; i++) {
+                result.push(trytesToAscii(fetched[i].message));
+            }
+        }
+        return result;
+    } catch (error) {
+        console.error('MAM fetch', error);
+        throw new Error(error);
+    }
+};
