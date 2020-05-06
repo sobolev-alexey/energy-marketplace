0a7f0ffb19a0b0804a8521978f77f1618e5a2e68
Alexey Sobolev <sobolev@mail.de>
2020-05-06 18:47:46 +0200

    Removed unused path

--- marketplace/src/index.ts
+++ marketplace/src/index.ts
@@ -10,7 +10,6 @@ const routes: IRoute[] = [
     { path: '/request', method: 'post', func: 'offerOrRequest' },
     { path: '/offer', method: 'post', func: 'offerOrRequest' },
     { path: '/match', method: 'post', func: 'match' }
-    // { path: '/v0/fetch', method: 'post', folder: 'v0', func: 'fetch' }
 ];
 
 AppHelper.build(routes, async (app, config, websocketPort) => {
