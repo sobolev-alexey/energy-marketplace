cdd30497351c9be2f0ffce7c1675a286f98180a7
Alexey Sobolev <sobolev@mail.de>
2020-05-07 12:10:06 +0200

    Added helper function to automate decryption and verification

---
+++ asset_consumer/src/utils/routineHelper.ts
@@ -0,0 +1,86 @@
+import { readData } from './databaseHelper';
+import { log } from './loggerHelper';
+import { publish } from './mamHelper';
+import { EncryptionService, IMessagePayload, IReceivedMessagePayload } from './encryptionHelper';
+import { sendRequest } from './communicationHelper';
+
+export async function decryptVerify(request: any): Promise<{ verificationResult: boolean, message?: any }> {
+    try {
+        if (request && request?.encrypted) {
+            const keys: any = await readData('keys');
+            if (keys && keys.privateKey) {
+                const encryptionService = new EncryptionService();
+
+                const decrypted: IReceivedMessagePayload = encryptionService.privateDecrypt(
+                    keys.privateKey, request?.encrypted
+                );
+    
+                const verificationResult: boolean = encryptionService.verifySignature(
+                    decrypted?.message?.assetPublicKey, decrypted?.message, decrypted?.signature
+                );  
+                    
+                const assetId = decrypted?.message?.assetId;
+                if (verificationResult) {
+                    await log(`Asset registration successful. ${assetId}`);
+                    return { verificationResult: true, message: decrypted?.message };
+                }
+                await log(`Asset signature verification failed. ${assetId}`);
+                return { verificationResult: false };
+            }
+            await log(`No marketplace key`);
+            throw new Error('No marketplace key');
+        } else {
+            await log(`No encrypted payload found`);
+            throw new Error('No encrypted payload found');
+        }
+    } catch (error) {
+        await log(`decryptVerify failed. ${error.toString()}`);
+        throw new Error(error);
+    }
+}
+
+export async function signPublishEncryptSend(
+    payload: object, 
+    assetId: string,
+    transactionId: string,
+    endpoint: string
+): Promise<{success: boolean}> {
+    try {
+        const asset: any = await readData('asset', 'assetId', assetId);
+
+        if (asset) {
+            // Retrieve encryption keys
+            const keys: any = await readData('keys');
+            if (!keys || !keys.privateKey) {
+                throw new Error('No keypair found in database');
+            }
+
+            // Sign payload
+            const encryptionService = new EncryptionService();
+            const signature: Buffer = encryptionService.signMessage(
+                keys?.privateKey, payload
+            );
+
+            // Publish payload to MAM
+            const mam = await publish(transactionId, { message: payload, signature });
+            console.log(333, mam);
+
+            // Encrypt payload and signature with asset's public key
+            const messagePayload: IMessagePayload = { message: payload, signature, mam };
+            console.log(444, messagePayload);
+
+            const encrypted: string = encryptionService.publicEncrypt(
+                asset?.assetPublicKey, JSON.stringify(messagePayload)
+            );
+
+            // Send encrypted payload and signature to asset
+            const response = await sendRequest(`${asset?.assetURL}/${endpoint}`, { encrypted });
+            console.log(555, response);
+            return response;
+        }
+    } catch (error) {
+        console.error('signPublishEncryptSend', error);
+        await log(`signPublishEncryptSend Error ${error.toString()}`);
+        return null;
+    }
+}
