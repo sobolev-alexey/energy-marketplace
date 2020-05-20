82fb8a82faef988871e116214e270cc515d49aa1
Alexey Sobolev <sobolev@mail.de>
2020-05-20 21:08:55 +0200

    Added payment processing route

--- marketplace/src/index.ts
+++ marketplace/src/index.ts
@@ -10,7 +10,8 @@ const routes: IRoute[] = [
     { path: '/request', method: 'post', func: 'offerOrRequest' },
     { path: '/offer', method: 'post', func: 'offerOrRequest' },
     { path: '/match', method: 'post', func: 'match' },
-    { path: '/provision', method: 'post', func: 'provision' }
+    { path: '/provision', method: 'post', func: 'provision' },
+    { path: '/payment_processing', method: 'post', func: 'payment_processing' },
 ];
 
 AppHelper.build(routes, async (app, config, websocketPort) => {
