f1354f4a729594c620b1818e34750a9543bebf1a
Alexey Sobolev <sobolev@mail.de>
2020-05-25 22:11:10 +0200

    Helper fuction to calculate max allowed timestamp

--- asset/src/utils/helpers.ts
+++ asset/src/utils/helpers.ts
@@ -1,5 +1,6 @@
 import { trytesToAscii } from '@iota/converter';
 import * as crypto from 'crypto';
+import { maxPendingDurationHours } from '../config.json';
 
 export const decodeMessage = fragment => {
     // Modify to consumable length
@@ -20,4 +21,10 @@ export const getHash = (message: string | object) => {
     const hasher: crypto.Hash = crypto.createHash('sha256');
     hasher.update(typeof message === 'string' ? message : JSON.stringify(message, null, 2));
     return hasher.digest('base64');
+};
+
+export const getMaxAllowedTimestamp = () => {
+    const currentTimestamp = Date.now();
+    const maxAllowedPendingTime = maxPendingDurationHours * 3600000; // Milliseconds in 1 hour multiplied by hours
+    return currentTimestamp - maxAllowedPendingTime;
 };
