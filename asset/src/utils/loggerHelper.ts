bae2116ddb932629ef35f526c8d7e201e440a1fe
Alexey Sobolev <sobolev@mail.de>
2020-05-25 22:10:40 +0200

    Prepare for transaction notification

--- asset/src/utils/loggerHelper.ts
+++ asset/src/utils/loggerHelper.ts
@@ -1,4 +1,5 @@
 import { writeData } from './databaseHelper';
+// import { signPublishEncryptSend } from './routineHelper';
 
 export const log = async (event: string) => {
     try {
@@ -13,6 +14,9 @@ export const transactionLog = async (payload: object) => {
     try {
         await writeData('transaction', payload);
         // console.log('Transaction log:', payload);
+
+        // Notify asset owner about new transaction event
+        // await signPublishEncryptSend(payload, 'notify');
     } catch (error) {
         console.error('logger', error);
     }
