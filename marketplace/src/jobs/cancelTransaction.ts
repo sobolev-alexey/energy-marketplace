427deb8932b06db0794db7bfc9d02166914da14b
Alexey Sobolev <sobolev@mail.de>
2020-06-05 23:15:49 +0200

    Cancellation job

---
+++ marketplace/src/jobs/cancelTransaction.ts
@@ -0,0 +1,52 @@
+import { decryptVerify, signPublishEncryptSend } from '../utils/routineHelper';
+import { log, transactionLog } from '../utils/loggerHelper';
+import { sendRequest } from '../utils/communicationHelper';
+import { bidManagerURL } from '../config.json';
+
+export default async (job, done) => {
+    try {
+        await log(`cancelTransaction job started`);
+        const request = await decryptVerify(job?.data);
+        const cancellationPayload = request?.message;
+
+        console.log('processCancellationRequest', request);
+        if (request?.verificationResult && cancellationPayload) {
+            // Log transaction
+            await transactionLog(cancellationPayload);
+
+            if (cancellationPayload?.contractId) {
+                let cancellationResponse;
+
+                if (cancellationPayload?.type === 'offer') {
+                    // Cancel for requester
+                    cancellationResponse = await signPublishEncryptSend(
+                        cancellationPayload, cancellationPayload?.requesterId, cancellationPayload?.requesterTransactionId, 'cancel'
+                    );
+                } else if (cancellationPayload?.type === 'request') {
+                    // Cancel for provider
+                    cancellationResponse = await signPublishEncryptSend(
+                        cancellationPayload, cancellationPayload?.providerId, cancellationPayload?.providerTransactionId, 'cancel'
+                    );
+                }
+
+                // Evaluate response
+                if (cancellationResponse?.success) {
+                    await log(`Transaction cancellation successful. ${request?.message?.contractId}`);
+                } else {
+                    await log(`Transaction cancellation request failure. Request: ${JSON.stringify(cancellationPayload)}, Response: ${JSON.stringify(cancellationResponse)}`);
+                }
+            } else {
+                // Cancel with Bid manager
+                const bidManagerEndpoint = `${bidManagerURL}/remove`;   
+                await sendRequest(bidManagerEndpoint, cancellationPayload);
+            }
+            done(null);
+        }
+        await log('Asset signature verification failed');
+        done(new Error('Asset signature verification failed'));
+    } catch (error) {
+        console.error('cancelTransaction', error);
+        await log(`cancelTransaction Error ${error.toString()}`);
+        done(new Error(`cancelTransaction Error ${error.toString()}`));
+    }
+};
