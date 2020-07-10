9e1d9024f5be1bda80140d14d6e0da3ec7c6c90e
Alexey Sobolev <sobolev@mail.de>
2020-07-11 00:27:33 +0200

    Updated modal

--- user/src/components/CustomModal.js
+++ user/src/components/CustomModal.js
@@ -1,32 +1,27 @@
 import React, { useState } from "react";
 
-import { Modal, Button } from "antd";
+import { Modal } from "antd";
 import { ExclamationCircleOutlined } from "@ant-design/icons";
 
-const CustomModal = ({ error, errorMessage }) => {
-  const [showModal, setShowModal] = useState(false);
+const CustomModal = ({ error, callback = null, show = false }) => {
+  const [showModal, setShowModal] = useState(show);
 
   return (
-    <div>
-      <Button type="primary" onClick={() => setShowModal(true)}>
-        Modal
-      </Button>
-      <Modal
-        title={
-          <span className="warning-text">
-            <ExclamationCircleOutlined className="warning-icon" /> Error Message
-          </span>
-        }
-        visible={showModal}
-        onCancel={() => setShowModal(false)}
-        cancelText="OK"
-        okButtonProps={{ disabled: true, className: "modal-footer-hidden-button" }}
-      >
-        <div className="modal-content">
-          <p>Something terrible happened and here we explain why it happened and how yoy can fix it</p>
-        </div>
-      </Modal>
-    </div>
+    <Modal
+      title={
+        <span className="warning-text">
+          <ExclamationCircleOutlined className="warning-icon" /> Error
+        </span>
+      }
+      visible={showModal}
+      onCancel={() => setShowModal(false) || callback }
+      cancelText="OK"
+      okButtonProps={{ disabled: true, className: "modal-footer-hidden-button" }}
+    >
+      <div className="modal-content">
+        <p>{ error }</p>
+      </div>
+    </Modal>
   );
 };
 
