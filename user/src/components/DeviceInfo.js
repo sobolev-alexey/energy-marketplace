a820ec52db2b23d9d47861fa304cb119b37b2099
Alexey Sobolev <sobolev@mail.de>
2020-07-11 18:54:52 +0200

    Updated device info to use real data

--- user/src/components/DeviceInfo.js
+++ user/src/components/DeviceInfo.js
@@ -1,11 +1,39 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { Card, Space, Row, Col } from "antd";
 import { PlayCircleOutlined, PauseCircleOutlined } from "@ant-design/icons";
+import { Loading } from ".";
 
 const { Meta } = Card;
 
-const DeviceInfo = ({ device }) => {
+const DeviceInfo = ({ device, transactions }) => {
+  const [energy, setEnergy] = useState();
+  const [total, setTotal] = useState();
+  const [price, setPrice] = useState();
+
+  useEffect(() => {
+    const transactionsCount = transactions && Object.keys(transactions)?.length;
+    if (transactions && transactionsCount > 0) {
+      setTotal(transactionsCount);
+
+      let totalEnergy = 0;
+      let totalPrice = 0;
+      let totalCount = 0;
+      Object.values(transactions).forEach(array => {
+        array.find(entry => {
+          if (entry?.status === 'Energy provision finished') {
+            totalEnergy += Number(entry?.energyAmount);
+            totalPrice += Number(entry?.energyPrice);
+            totalCount++;
+          }
+        })
+      });
+      setEnergy(totalEnergy);
+      setPrice((totalPrice / totalCount || 0).toFixed(2));
+    }
+  }, [transactions]); // eslint-disable-line react-hooks/exhaustive-deps
+
+  console.log('transactions', transactions);
   return (
     <div className="device-info">
       <Row gutter={20}>
@@ -36,7 +64,7 @@ const DeviceInfo = ({ device }) => {
         <Col span={8}>
           <Card hoverable className="device-info-card">
             <span> DEVICE WALLET </span>
-            <h1 className="wallet-balance-device">
+            <h1 className="transaction-info-device">
               { device?.wallet?.balance } <span className="wallet-balance3-device"> Iota </span>
             </h1>
             <div>
@@ -55,22 +83,47 @@ const DeviceInfo = ({ device }) => {
       <Row gutter={16}>
         <Col span={8}>
           <Card hoverable className="device-info-card">
-            <span> ENERGY PRODUCED </span>
-            <h1 className="wallet-balance-device">
-              575 <span className="wallet-balance3-device"> kWh </span>
+            { 
+              device?.type === 'requester'
+              ? <span> ENERGY CONSUMED </span>
+              : <span> ENERGY PRODUCED </span>
+            }
+            <h1 className="transaction-info-device">
+              { !energy ? (
+                <Loading />
+              ) : (
+                <React.Fragment>
+                  { energy } <span className="wallet-balance3-device"> kWh </span>
+                </React.Fragment>
+              )}
             </h1>
           </Card>
         </Col>
         <Col span={8}>
           <Card hoverable className="device-info-card">
-            <span> TOTAL TRANSACTIONS </span> <h1 className="wallet-balance-device"> 483 </h1>
+            <span> TOTAL TRANSACTIONS </span> 
+            <h1 className="transaction-info-device"> 
+              { !total ? (
+                <Loading />
+              ) : (
+                <React.Fragment>
+                  { total }
+                </React.Fragment>
+              )}
+            </h1>
           </Card>
         </Col>
         <Col span={8}>
           <Card hoverable className="device-info-card">
             <span> AVERAGE ENERGY PRICE </span>
-            <h1 className="wallet-balance-device">
-              1.28 <span className="wallet-balance3-device"> Mi </span>
+            <h1 className="transaction-info-device">
+              { !total ? (
+                <Loading />
+              ) : (
+                <React.Fragment>
+                  { price } <span className="wallet-balance3-device"> Iota </span>
+                </React.Fragment>
+              )}
             </h1>
           </Card>
         </Col>
