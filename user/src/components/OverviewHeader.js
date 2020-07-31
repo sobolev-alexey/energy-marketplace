72427409ac317d169326217a67415aa927b4d1fe
Alexey Sobolev <sobolev@mail.de>
2020-07-31 16:19:30 +0200

    Fixed header

--- user/src/components/OverviewHeader.js
+++ user/src/components/OverviewHeader.js
@@ -3,12 +3,20 @@ import { Link } from "react-router-dom";
 
 import { PlusCircleFilled } from "@ant-design/icons";
 
-const OverviewHeader = () => (
+const OverviewHeader = ({ marketplace = false }) => (
   <div className="header-wrapper2">
-    <h5> Devices </h5>
-    <Link to="/new" className="cta2">
-      <PlusCircleFilled style={{ fontSize: "36px" }} /> Add a new device <span> </span>
-    </Link>
+    {
+      marketplace ? (
+          <h5>Marketplace</h5>
+      ) : (
+        <React.Fragment>
+          <h5>Devices</h5>
+          <Link to="/new" className="cta2">
+            <PlusCircleFilled style={{ fontSize: "36px" }} /> Add a new device
+          </Link>
+        </React.Fragment>
+      )
+    }
   </div>
 );
 
