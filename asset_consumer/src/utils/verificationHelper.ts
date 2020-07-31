0196dfb8388ff10760c24a6a925f16d61408e4ce
Alexey Sobolev <sobolev@mail.de>
2020-07-31 17:47:56 +0200

    Clean-up

--- asset_consumer/src/utils/verificationHelper.ts
+++ asset_consumer/src/utils/verificationHelper.ts
@@ -5,13 +5,11 @@ import { EncryptionService } from './encryptionHelper';
 
 export async function verifyRequest(request: any): Promise<any> {
     try {
-        console.log('verifyRequest 01', request?.status);
         // Check request status
         if (request?.status !== 'Payment requested') {
             return false;
         }
 
-        console.log('verifyRequest 02', request?.type);
         // Get transaction from transaction log
         const transactionId = request?.type === 'offer' 
             ? request.providerTransactionId 
@@ -21,39 +19,26 @@ export async function verifyRequest(request: any): Promise<any> {
             ? await readAllData('transactionLog', null, 'providerTransactionId', transactionId)
             : await readAllData('transactionLog', null, 'requesterTransactionId', transactionId);
         
-        console.log('verifyRequest 03', transactionId);
-
         transactions.sort((a, b) => Number(a.timestamp) - Number(b.timestamp));
-
-        console.log('verifyRequest 04', transactions.length);
 
         if (!transactionId || !transactions.length) {
             return false;
         }
 
-        console.log('verifyRequest 05');
-
         // Get transaction from MAM
         let mamTransactions: any = await fetch(transactionId);
         if (!mamTransactions.length) {
-            console.log('verifyRequest 666', mamTransactions.length);
             return false;
         }
-
-        console.log('verifyRequest 06', mamTransactions.length);
 
         mamTransactions = mamTransactions
             .map(transaction => JSON.parse(transaction))
             .sort((a, b) => Number(a?.message?.timestamp) - Number(b?.message?.timestamp));
         
-        console.log('verifyRequest 07', mamTransactions);
-
         const initialTransaction = transactions.find(transaction => transaction.status === 'Initial request');
         const initialTransactionMam = mamTransactions.find(transaction => transaction?.message?.status === 'Initial request');
         const contractTransaction = transactions.find(transaction => transaction.status === 'Contract created');
         
-        console.log('verifyRequest 08');
-
         // Check signature of the first mam message
         const encryptionService = new EncryptionService();
         const keys: any = await readData('keys');
@@ -61,40 +46,25 @@ export async function verifyRequest(request: any): Promise<any> {
             keys.publicKey, initialTransactionMam?.message, initialTransactionMam?.signature
         ); 
 
-        console.log('verifyRequest 09', verificationResult);
-
         if (!verificationResult) {
             return false;
         }
 
-        console.log('verifyRequest 10', initialTransaction, initialTransactionMam?.message);
         // Compare transaction object props
         if (!compareObjectProps(initialTransaction, initialTransactionMam?.message, ['contractId', 'requesterTransactionId', 'providerTransactionId', 'energyAmount', 'energyPrice' ])) {
-            console.log('verifyRequest 666 10');
-            
             return false;
         }
-
-        console.log('verifyRequest 11', initialTransaction, request);
 
         if (!compareObjectProps(initialTransaction, request, ['requesterTransactionId', 'energyAmount', 'location' ])
             || (initialTransaction?.energyPrice < request?.energyPrice)) {
-            console.log('verifyRequest 666 11');
-            
             return false;
         }
-
-        console.log('verifyRequest 13', contractTransaction, request);
 
         if (!compareObjectProps(contractTransaction, request, ['contractId', 'requesterTransactionId', 'providerTransactionId', 'energyAmount', 'walletAddress', 'providerId', 'location' ])
             || (contractTransaction?.energyPrice < request?.energyPrice)) {
-            console.log('verifyRequest 666 13');
-            
             return false;
         }
     
-        console.log('verifyRequest SUCCESS');
-
         return true;
     } catch (error) {
         await log(`verifyRequest failed. ${JSON.stringify(error)}`);
